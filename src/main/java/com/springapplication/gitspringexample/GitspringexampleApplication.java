92083526baf38e7f54e40736b53bbbe259d97881
nagalakshmi961 <nagalakshmi.mekala@gmail.com>
2020-08-12 18:14:39 +0530

    context added

--- src/main/java/com/springapplication/gitspringexample/GitspringexampleApplication.java
+++ src/main/java/com/springapplication/gitspringexample/GitspringexampleApplication.java
@@ -3,12 +3,14 @@ package com.springapplication.gitspringexample;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GitspringexampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GitspringexampleApplication.class, args);
+		ConfigurableApplicationContext context=SpringApplication.run(GitspringexampleApplication.class, args);
+	context.close();
 	}
 
 }
