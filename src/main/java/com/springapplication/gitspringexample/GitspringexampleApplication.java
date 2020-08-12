2b611183366dddb36358ca197961d718d04ef401
nagalakshmi961 <nagalakshmi.mekala@gmail.com>
2020-08-12 15:51:45 +0530

    init local commit

---
+++ src/main/java/com/springapplication/gitspringexample/GitspringexampleApplication.java
@@ -0,0 +1,14 @@
+package com.springapplication.gitspringexample;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitspringexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitspringexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springapplication/gitspringexample/GitspringexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapplication.gitspringexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitspringexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
